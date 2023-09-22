aa2f9c220a3676da4661687d125fe5aa7da6f219
rafael <rafaelsantossgaldino2@gmail.com>
2023-09-22 19:52:00 -0300

    feature: reestructure model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,13 @@
 class User < ApplicationRecord
-  has_many :permissons
-
-  def activate!(reason)
-    update(reason: reason, active: true)
-  end
-
-  def desactivate!(reason)
-    update(reason: reason, active: false)
-  end
+  has_many :permissions
 
   def set_permission(permission_name)
-    permission_name.create(name: permission_name)
+    permissions.create(name: permission_name)
+    activate!("because #{permission_name} was created")
   end
 
   def remove_permission(permission_name)
     permissions.find_by(name: permission_name).delete
+    desactivate!("because #{permission_name} was removed")
   end
 end
