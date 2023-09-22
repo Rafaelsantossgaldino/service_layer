328a6f4d810489e23c3b665f18e025c681c089de
rafael <rafaelsantossgaldino2@gmail.com>
2023-09-22 18:46:03 -0300

    obs_commit: add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_22_212523) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_22_214025) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "permissions", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
