5768a84af7e30d79b389904a0591d6f9dcc9383e
rafael <rafaelsantossgaldino2@gmail.com>
2023-09-22 18:57:57 -0300

    feature: struicture model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+  has_many :permissons
+
+  def activate!(reason)
+    update(reason: reason, active: true)
+  end
+
+  def desactivate!(reason)
+    update(reason: reason, active: false)
+  end
+
+  def set_permission(permission_name)
+    permission_name.create(name: permission_name)
+  end
+
+  def remove_permission(permission_name)
+    permissions.find_by(name: permission_name).delete
+  end
 end
