8100bdbccc12504be75ea1b068f12786219169af
rafael <rafaelsantossgaldino2@gmail.com>
2023-09-22 19:52:35 -0300

    feature: create service user

---
+++ app/services/users/activate_service.rb
@@ -0,0 +1,20 @@
+module Users
+  class ActivateService
+    def initialize(user)
+      @user = user
+    end
+
+    def call(reason)
+      activate!(reason)
+    end
+
+    private
+    
+    def activate!(reason)
+      @user.update(reason: reason, active: true)
+    end
+  end
+end
+
+
+#Users::Activateservice.new(user).call
