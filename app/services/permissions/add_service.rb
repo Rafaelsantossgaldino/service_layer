531e353f8080dfebdf9cb150717eb6ec1b05ed8e
rafael <rafaelsantossgaldino2@gmail.com>
2023-09-22 20:13:36 -0300

    feature: create add_service

---
+++ app/services/permissions/add_service.rb
@@ -0,0 +1,17 @@
+module Permissions
+  class AddService
+    def initialize(user)
+      @user = user
+    end
+
+    def call(name)
+      add(name)
+    end
+
+    private
+
+    def add(name)
+      @user.permissions.create(name: name)
+    end
+  end
+end
